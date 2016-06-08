79ca8fa5d3b99c44ca31c88c5da9d09930708d86
George LEOTESCU <gleotescu@bbu.bitdefender.biz>
2016-06-08 09:44:49 +0300

    pushd missing files

---
+++ FaceNormalizationService/Service/models/descompose.m
@@ -0,0 +1,9 @@
+function [x,y,z] = decompose_rotation(R)
+	x = atan2(R(3,2), R(3,3));
+	y = atan2(-R(3,1), sqrt(R(3,2)*R(3,2) + R(3,3)*R(3,3)));
+	z = atan2(R(2,1), R(1,1));
+    
+    x = x * 180 / pi;
+    y = y * 180 / pi;
+    z = z * 180 / pi;
+end
